e1653d772cd7fa7bdd933db1ca7273b87f953e24
Hans Baier <foss@hans-baier.de>
2024-05-08 11:41:11 +0700

    add _Utility cores

--- sync.sh
+++ sync.sh
@@ -4,3 +4,4 @@ rsync -avu root@mistex.lan:/media/fat/_Arcade_A200T484 .
 rsync -avu root@mistex.lan:/media/fat/_Arcade_A200T676 .
 rsync -avu root@mistex.lan:/media/fat/_Arcade_K325T .
 rsync -avu root@mistex.lan:/media/fat/_Console .
+rsync -avu root@mistex.lan:/media/fat/_Utility .
