71a6ffab36d3da102c84ddbcde83778740ef6bd5
Hans Baier <foss@hans-baier.de>
2024-05-08 11:37:27 +0700

    Add A200T484 bitstreams

---
+++ _Arcade_A200T484/mame/README.txt
@@ -0,0 +1,1 @@
+put your MAME rom files here
--- sync.sh
+++ sync.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rsync -avu root@mistex.lan:/media/fat/_Arcade_A100T .
+rsync -avu root@mistex.lan:/media/fat/_Arcade_A200T484 .
+rsync -avu root@mistex.lan:/media/fat/_Arcade_A200T676 .
 rsync -avu root@mistex.lan:/media/fat/_Arcade_K325T .
 rsync -avu root@mistex.lan:/media/fat/_Console .
